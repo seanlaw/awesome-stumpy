5c234380fb77238568ac43a11ea3f39c62709386
Sean Law <seanmylaw@gmail.com>
2023-06-21 10:08:38 -0400

    Fixed bug

--- docs/conf.py
+++ docs/conf.py
@@ -31,9 +31,9 @@ copyright = (
 author = "Sean M. Law"
 
 # The short X.Y version
-version = 0.1.0
+version = "0.1.0"
 # The full version, including alpha/beta/rc tags
-release = 0.1.0
+release = version
 
 
 # -- General configuration ---------------------------------------------------
